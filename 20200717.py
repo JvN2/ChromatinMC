ece5e9c8848c5b7191de160e36c45a680b0135f7
AnneliesVlaar <a.m.vlaar@umail.leidenuniv.nl>
2021-01-13 10:27:04 +0100

    small changes

--- 20200717.py
+++ 20200717.py
@@ -13,10 +13,10 @@ import FileIO as fileio
 
 # TA.plot_npz(r"D:\Downloads\7_91nm\8x197x2s102w2-5_001")
 
-# TA.repulsion_exp()
+TA.repulsion_exp()
 # TA.expo_decay()
 # TA.debye()
-TA.tail_energy()
+# TA.tail_energy()
 # TA.get_stack_params(r"C:\Users\Annelies\OneDrive\Documents\experimental data\20201218 Fixed Parameters Stacking\8x197x2s2500000w2-5_001")
 # TA.get_stack_params(r"D:\Downloads\20201218 Fixed Parameters Stacking\8x197x0s2500000w2-5_001")
 # print(TA.get_stack_params(r"C:\Users\Annelies\OneDrive\Documents\experimental data\20201208 fixed parameters stacking\8x167x0s102w79-1_001.xlsx"))
@@ -35,11 +35,12 @@ TA.tail_energy()
 # TA.wrap(r"C:\Users\Annelies\OneDrive\Documents\experimental data\20201216 Eunwrap\FPS\8x167x1s2500000w5-5_001")
 
 
-# # get list of files
-# files = glob.glob(r"C:\Users\Annelies\OneDrive\Documents\experimental data\20201216 Eunwrap\7_91\8x197x2s102w*_001")
-#
+# # # get list of files
+# files = glob.glob(r"D:\Users\vlaar\data\20210105\8x1*7x*s102w*_001")
+# # "D:\Users\vlaar\data\20210105\8x1*7x*s102w*-0_001"
+# #
 # for f in files:
-#     # print(f)
-#     TA.wrap(f)
+#     print(f)
+#     TA.rep(f)
 
 # TA.plot_wrap(197, 2)
